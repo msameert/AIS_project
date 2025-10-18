e8903de658f6058b44e78ce363c0bf8cfb3201e4
msameert <msameert123@gmail.com>
2025-10-18 22:05:25 +0500

    Frontend homepage

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="frontend/templates",static_folder="frontend/static")
 
 @app.route('/')
-def index():
-    return "Hello world"
+def base():
+    return render_template("base.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
